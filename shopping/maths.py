5b806237f313e3d0877b03d5249f0c835e7d95ea
Tarun <tarunchaudhary1903@gmail.com>
2019-10-07 16:01:07 +0200

    now slide is working fine like before

---
+++ shopping/maths.py
@@ -0,0 +1,24 @@
+# from django.shortcuts import render
+
+# x = 'how are you tarun'
+# print(x)
+#
+# print('hellp [elp')
+#
+# x = input('Enter a number\n')
+# print(type(x))
+#
+# y = 90
+# print(type(y))
+
+
+dic1 = {'tarun': 'smart handsome guy', 'Ashi': 'lazyass young girl', 'weather': 'amazing with sun'}
+input_w = input('insert a word\n')
+print(dic1[input_w])
+
+
+
+
+
+
+
